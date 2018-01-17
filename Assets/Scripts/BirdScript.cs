b416da23d75c952ba2c8e82f78d9090e6f71f5d0
Josue David Valenzuela Herrera <val171001@uvg.edu.gt>
2018-01-17 08:46:03 -0600

    Avances paso 2.8

---
+++ Assets/Scripts/BirdScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour {
+
+	public float jumpForce = 10;
+
+	// Use this for initialization
+	void Start () {	
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Jump")){
+			Debug.Log("Espacio");
+		}
+
+	}
+}
