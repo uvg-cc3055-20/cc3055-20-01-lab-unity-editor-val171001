d6705e0e0af7445be56695fbfdf5489d91ec709b
Josue David Valenzuela Herrera <val171001@uvg.edu.gt>
2018-01-17 16:20:04 -0600

    Moving scene

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -4,17 +4,30 @@ using UnityEngine;
 
 public class BirdScript : MonoBehaviour {
 
-	public float jumpForce = 10;
+	public float jumpForce;
+	public Rigidbody2D rb;
+	public float forwardSpeed;
+	public GameObject cam;
 
 	// Use this for initialization
 	void Start () {	
-		
+		jumpForce = 300f;
+		rb = GetComponent<Rigidbody2D> ();
+		forwardSpeed = 2f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		// Moverse horizontalmente
+		rb.transform.Translate(new Vector3(1,0,0) * forwardSpeed * Time.deltaTime);
+
+		// Camara siga al personaje
+		cam.transform.Translate(new Vector3(1,0,0) * forwardSpeed * Time.deltaTime);
+
+		// Saltar al presionar la barra espaciadora
 		if(Input.GetButtonDown("Jump")){
-			Debug.Log("Espacio");
+			rb.velocity = Vector2.zero;
+			rb.AddForce (Vector2.up * jumpForce);
 		}
 
 	}
