cfe1cc0a3da15426e334311a23438e278defb350
Josue David Valenzuela Herrera <val171001@uvg.edu.gt>
2018-01-17 18:16:51 -0600

    Pasos 2.14

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -8,27 +8,39 @@ public class BirdScript : MonoBehaviour {
 	public Rigidbody2D rb;
 	public float forwardSpeed;
 	public GameObject cam;
+	public bool dead;
 
 	// Use this for initialization
 	void Start () {	
 		jumpForce = 300f;
 		rb = GetComponent<Rigidbody2D> ();
 		forwardSpeed = 2f;
+		dead = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		// Moverse horizontalmente
-		rb.transform.Translate(new Vector3(1,0,0) * forwardSpeed * Time.deltaTime);
+		// Verificarr que siga vivo
+		if (dead.Equals (false)) {
+			// Moverse horizontalmente
+			rb.transform.Translate (new Vector3 (1, 0, 0) * forwardSpeed * Time.deltaTime);
 
-		// Camara siga al personaje
-		cam.transform.Translate(new Vector3(1,0,0) * forwardSpeed * Time.deltaTime);
+			// Camara siga al personaje
+			cam.transform.Translate (new Vector3 (1, 0, 0) * forwardSpeed * Time.deltaTime);
 
-		// Saltar al presionar la barra espaciadora
-		if(Input.GetButtonDown("Jump")){
-			rb.velocity = Vector2.zero;
-			rb.AddForce (Vector2.up * jumpForce);
+			// Saltar al presionar la barra espaciadora
+			if (Input.GetButtonDown ("Jump")) {
+				rb.velocity = Vector2.zero;
+				rb.AddForce (Vector2.up * jumpForce);
+			}
 		}
 
 	}
+
+	// Defeat function
+	private void OnCollisionEnter2D(Collision2D collision){
+		dead = false;
+	}
+
+
 }
