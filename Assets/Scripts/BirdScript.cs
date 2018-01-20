8c9affaa68d59dd98d5a97eeda47ecf34ea6f83f
Josue David Valenzuela Herrera <val171001@uvg.edu.gt>
2018-01-19 20:27:29 -0600

    Upadtes:
    - Added defeat function and end of level
    - Added coins that the player can collect

--- Assets/Scripts/BirdScript.cs
+++ Assets/Scripts/BirdScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BirdScript : MonoBehaviour {
 
+	// GameObjects and variables to used
 	public float jumpForce;
 	public Rigidbody2D rb;
 	public float forwardSpeed;
@@ -12,9 +13,18 @@ public class BirdScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {	
+		// Force used to jump
 		jumpForce = 300f;
+
+		// Getting the character
 		rb = GetComponent<Rigidbody2D> ();
+		// Freezing character rotation
+		rb.freezeRotation = true;
+
+		// Level speed
 		forwardSpeed = 2f;
+
+		// Starts alive
 		dead = false;
 	}
 	
@@ -28,7 +38,7 @@ public class BirdScript : MonoBehaviour {
 			// Camera follows character
 			cam.transform.Translate (new Vector3 (1, 0, 0) * forwardSpeed * Time.deltaTime);
 
-			// Jump pressing the space bar
+			// Jump by pressing the space bar
 			if (Input.GetButtonDown ("Jump")) {
 				rb.velocity = Vector2.zero;
 				rb.AddForce (Vector2.up * jumpForce);
@@ -42,10 +52,15 @@ public class BirdScript : MonoBehaviour {
 
 	}
 
-	// Defeat function
+	// Defeat & collision function
 	private void OnCollisionEnter2D(Collision2D collision){
-		dead = true;
+		// If player collide with a coin
+		if (collision.gameObject.tag == "coin") {
+			Destroy (collision.gameObject);
+		} else { // Player collide with other object
+			dead = true;
+		}
 	}
-
+		
 
 }
